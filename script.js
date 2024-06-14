5e4fdce831c3e4e24b274e5bf7f5771d773a58b0
MiladMesbahi <Milad.n.mesbahi@gmail.com>
2024-06-13 18:56:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -159,18 +159,3 @@ for (let i = 0; i < navigationLinks.length; i++) {
 }
 
 
-// Add event to all nav link
-for (let i = 0; i < navigationLinks.length; i++) {
-    navigationLinks[i].addEventListener("click", function () {
-        for (let j = 0; j < pages.length; j++) {
-            if (this.innerHTML.toLowerCase() === pages[j].dataset.page) {
-                pages[j].classList.add("active");
-                navigationLinks[i].classList.add("active");
-                window.scrollTo(0, 0);
-            } else {
-                pages[j].classList.remove("active");
-                navigationLinks[j].classList.remove("active");
-            }
-        }
-    });
-}
