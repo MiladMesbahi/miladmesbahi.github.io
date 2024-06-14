f1cdc6b83561d7989726147d3ccf3f19861ebd95
MiladMesbahi <Milad.n.mesbahi@gmail.com>
2024-06-13 18:51:07 -0700

    project fix

--- script.js
+++ script.js
@@ -157,3 +157,20 @@ for (let i = 0; i < navigationLinks.length; i++) {
 
     });
 }
+
+
+// Add event to all nav link
+for (let i = 0; i < navigationLinks.length; i++) {
+    navigationLinks[i].addEventListener("click", function () {
+        for (let j = 0; j < pages.length; j++) {
+            if (this.innerHTML.toLowerCase() === pages[j].dataset.page) {
+                pages[j].classList.add("active");
+                navigationLinks[i].classList.add("active");
+                window.scrollTo(0, 0);
+            } else {
+                pages[j].classList.remove("active");
+                navigationLinks[j].classList.remove("active");
+            }
+        }
+    });
+}
