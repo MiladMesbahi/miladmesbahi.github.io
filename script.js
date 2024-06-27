d3b1d66d9396b60cdb9d2394b1f0870d127c6b23
MiladMesbahi <Milad.n.mesbahi@gmail.com>
2024-06-27 00:11:55 -0700

    movie try

--- script.js
+++ script.js
@@ -14,41 +14,6 @@ sidebarBtn.addEventListener("click", function () {
     elementToggleFunc(sidebar);
 });
 
-// Testimonials variables
-const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
-const modalContainer = document.querySelector("[data-modal-container]");
-const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
-const overlay = document.querySelector("[data-overlay]");
-
-// Modal variable
-const modalImg = document.querySelector("[data-modal-img]");
-const modalTitle = document.querySelector("[data-modal-title]");
-const modalText = document.querySelector("[data-modal-text]");
-const pdfViewer = document.getElementById("pdf-viewer");
-
-// Modal toggle function
-const testimonialsModalFunc = function () {
-    modalContainer.classList.toggle("active");
-    overlay.classList.toggle("active");
-};
-
-// Add click event to all modal items
-testimonialsItem.forEach(item => {
-    item.addEventListener("click", function () {
-        modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
-        modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
-        modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
-        modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
-        const pdfSrc = this.querySelector(".open-modal-btn").getAttribute("data-pdf");
-        pdfViewer.setAttribute("src", pdfSrc);
-        testimonialsModalFunc();
-    });
-});
-
-// Add click event to modal close button
-modalCloseBtn.addEventListener("click", testimonialsModalFunc);
-overlay.addEventListener("click", testimonialsModalFunc);
-
 // Page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
